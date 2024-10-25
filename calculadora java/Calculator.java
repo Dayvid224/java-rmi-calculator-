9342f439f0e4fa3809703142d367c494a8d72625
Dayvid Sampaio <105394348+Dayvid224@users.noreply.github.com>
2024-10-25 00:28:12 -0300

    Calculator.java

--- calculadora java/Calculator.java
+++ calculadora java/Calculator.java
@@ -1,11 +1,7 @@
-public interface Calculator
-extends java.rmi.Remote {
-public float add(float a, float b)
-throws java.rmi.RemoteException;
-public float sub(float a, float b)
-throws java.rmi.RemoteException;
-public float mul(float a, float b)
-throws java.rmi.RemoteException;
-public float div(float a, float b)
-throws java.rmi.RemoteException;
+import java.rmi.Remote; 
+import java.rmi.RemoteException; 
+   
+public interface Calcutator extends Remote 
+{ 
+    public long add(long a, long b) throws RemoteException; 
 }
