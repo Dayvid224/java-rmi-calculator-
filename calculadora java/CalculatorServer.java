00c2ecada355af7b83a51b401d9062e00eb31e4f
Dayvid Sampaio <105394348+Dayvid224@users.noreply.github.com>
2024-10-25 00:27:47 -0300

    CalculatorServer.java

--- calculadora java/CalculatorServer.java
+++ calculadora java/CalculatorServer.java
@@ -1,14 +1,26 @@
 import java.rmi.Naming;
-public class CalculatorServer {
-public CalculatorServer() {
-try {
-Calculator c = new CalculatorImpl();
-Naming.rebind("rmi://localhost:1099/CalculatorService", c);
-} catch (Exception e) {
-System.out.println("Trouble: " + e);
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+
+public class CalculatorServer<Calculator> {
+
+    public CalculatorServer() {
+        try {
+            // Cria o registro RMI
+            LocateRegistry.createRegistry(1020);
+
+            final Calculator calculator = new CalculatorImpl();
+            Naming.rebind("rmi://127.0.0.1:1020/CalculatorService", (Remote) calculator);
+            System.out.println("Servidor RMI iniciado");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void main(String[] args) {
+        new CalculatorServer();
+    }
 }
-}
-public static void main(String args[]) {
-new CalculatorServer();
-}
-}
+
+
