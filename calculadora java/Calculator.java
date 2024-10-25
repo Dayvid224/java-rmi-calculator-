ab064ee2171da0726d14f9d1fecd43082503eae3
Dayvid Sampaio <105394348+Dayvid224@users.noreply.github.com>
2024-10-24 23:25:18 -0300

    Add files via upload

---
+++ calculadora java/Calculator.java
@@ -0,0 +1,11 @@
+public interface Calculator
+extends java.rmi.Remote {
+public float add(float a, float b)
+throws java.rmi.RemoteException;
+public float sub(float a, float b)
+throws java.rmi.RemoteException;
+public float mul(float a, float b)
+throws java.rmi.RemoteException;
+public float div(float a, float b)
+throws java.rmi.RemoteException;
+}
